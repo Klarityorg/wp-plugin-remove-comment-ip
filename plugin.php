4f8df87658e1d8077dc30d03d3a0bbde864e0ab4
Fredrik Areschoug <fredrik@klarity.org>
2019-03-12 11:48:02 +0100

    updated name

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name: Klarity remove ip
+ * Plugin Name: Klarity remove comments ip
  * Plugin URI: https://github.com/ahmadawais/create-guten-block/
- * Description: Klarity remove ip
+ * Description: Klarity remove comments ip
  * Author: Klarity
  * Author URI: https://github.com/Klarityorg
  * Version: 1.0.0
